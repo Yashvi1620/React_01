2a55f55a7ab62ab6be96ce791219ce8afe617e83
Yashvi Patwa <96307761+Yashvi1620@users.noreply.github.com>
2022-11-02 22:22:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Project from "./Project";
+
+function App() {
+  return (
+    <>
+    <Project/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Project.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Project.css"
+const Project = () => {
+  return (
+    <>
+    <div className="main">
+        <div className="para">
+
+        <div className='about'>About Us</div>
+        <br />
+        <p className='p1'>Lorem ipsum dolor sit amet, consectetur adipisicing elit consectetu
+       ree ea facere, rem nostru</p>
+        <br />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum unde expedita beatae aperiam commodi quia quam excepturi omnis placeat hic dolorem asperiores, consectetur dolorum blanditiis dolor nihil labore eveniet vero!
+         nostrum. Odio assumenda, neque mollitia libero facere quos voluptatem, praesentium veniam architecto magnam laborum dolorum quidem totam optio veritatis suscipit deserunt, fugit consectetur.
+        Deleniti libero atque veniam officia voluptas temporibus, harum iure similique ea accusamus ipsa quos optio <span className='dash'>______ .</span> 
+        </p>
+        <br />
+        <br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore modi fuga similique! Perferendis nesciunt quae illo autem facere ducimus accusamus rerum repellat laudantium, nemo voluptates inventore sunt id, eveniet incidunt.</p>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Project
